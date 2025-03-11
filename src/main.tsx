a8b934fe7626ff0af1e06c61f5caf5b7e56cf1aa
Maxim Dudai <maxim.dudai01@gmail.com>
2025-03-11 17:55:43 +0000

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 
 import LandingPage from './App'
@@ -11,16 +12,17 @@ import Archive from './pages/archive'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route index path="/" element={<LandingPage />} />
-      <Route path="/aboutme" element={<LandingPage />} />
-      <Route path="/experience" element={<LandingPage />} />
-      <Route path="/abilities" element={<LandingPage />} />
-      <Route path="/projects" element={<LandingPage />} />
-      <Route path="/certificates" element={<LandingPage />} />
-      <Route path="/education" element={<LandingPage />} />
-      <Route path="/archive" element={<Archive />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<LandingPage />} />
+        <Route path="/aboutme" element={<LandingPage />} />
+        <Route path="/experience" element={<LandingPage />} />
+        <Route path="/abilities" element={<LandingPage />} />
+        <Route path="/projects" element={<LandingPage />} />
+        <Route path="/certificates" element={<LandingPage />} />
+        <Route path="/education" element={<LandingPage />} />
+        <Route path="/archive" element={<Archive />} />
+      </Routes>
+    </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )
